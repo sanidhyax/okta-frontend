14a9e71bb4a49587584629719eded7d257decaea
Sanidhya <sanidhyax@gmail.com>
2024-04-23 04:10:26 +0530

    Added rating

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import "../App.css";
+import Rater from 'react-rater'
+import 'react-rater/lib/react-rater.css'
+import { Tooltip as ReactTooltip } from "react-tooltip";
+
+
 
 const ProductCard = (props) => {
-    const { image, brand, title, price, discountedPrice } = props;
+    const { image, brand, title, price, discountedPrice,rating, id } = props;
 
     return (
-        <a className="product-card" href="#dolce-gabbana-cropped">
+        <>
+        <a className="product-card" href="">
             <img className="product-card__image" src={image} />
+            <Rater data-tooltip-id={id} total={5} rating={rating} interactive={false} />
             <div id="product-card-details">
                 <div className="product-card-name-div">
                     <p className="product-card__brand">{brand}</p>
@@ -18,6 +25,12 @@ const ProductCard = (props) => {
                 </div>
             </div>
         </a>
+        <ReactTooltip
+        id={id}
+        place="bottom"
+        content={"Rating " + rating}
+      />
+        </>
     );
 };
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,7 +7,7 @@ const ProductList = (props) => {
 
     const listProducts = () => {
         if (products.length > 0){
-            return products.map((p) => <ProductCard key={p.id} image={p.images[0]} title={p.title} brand={p.brand} price={p.price} discountedPrice={p.discountedPrice}/>)
+            return products.map((p) => <ProductCard key={p.id} image={p.images[0]} title={p.title} brand={p.brand} price={p.price} discountedPrice={p.discountedPrice} rating={p.rating} id={p.id}/>)
         } else {
             return "No Products"
         }
