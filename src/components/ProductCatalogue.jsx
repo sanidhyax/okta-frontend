e1540661b34d22e2f95961cae48f386950fa6e63
Sanidhya <sanidhyax@gmail.com>
2024-05-01 04:35:55 +0530

    Update ProductCatalogue.jsx

--- src/components/ProductCatalogue.jsx
+++ src/components/ProductCatalogue.jsx
@@ -46,7 +46,6 @@ export const ProductCatalogue = () => {
     useEffect(() => {
         getCategories(categoriesURL)
         getBrands(brandUrl, '')
-        console.log(pagination)
         getProducts(baseUrlForProducts, pagination.skip, pagination.limit)
     }, [])
 
@@ -63,12 +62,11 @@ export const ProductCatalogue = () => {
             )
     };
 
-    const getBrands = (url, c) => {
-        console.log("check" + c)
+    const getBrands = (url, category) => {
         dispatch(setBrandsLoading(true));
         axios.get(url, {
             params: {
-                category: c
+                category: category
             }
         })
             .then(response => {
